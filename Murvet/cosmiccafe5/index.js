832f11f3fa09a05eafe4fe8e66d04a8c5f11ba61
agazademurvet <murvetagazade@gmail>
2022-07-17 10:02:25 +0400

    hazir

---
+++ Murvet/cosmiccafe5/index.js
@@ -0,0 +1,15 @@
+    const menuIcon = document.querySelector(".hamburger");
+    const navbar = document.querySelector(".navbar");  
+
+    menuIcon.addEventListener("click", () => {
+    navbar.classList.toggle("change");
+    
+});
+
+document.querySelector(".one").onclick =function(){
+    location.href ="../../Aziz/html/page2.html"
+}
+
+document.querySelector(".two").onclick =function(){
+    location.href ="../../Aziz/html/page2.html"
+}
